80db1d27b8e84a097a2a4979ff4b398758fd0953
shubham kumar <shubhofficial1@gmail.com>
2022-12-02 02:39:34 +0530

    react bootstrap,header,footer, cdnjs added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
