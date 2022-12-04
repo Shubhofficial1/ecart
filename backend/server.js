d3e7bdffe6f95159133742c3b6e1b6af13f8a914
shubham kumar <shubhofficial1@gmail.com>
2022-12-04 14:55:02 +0530

    Serve products from backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const products = require("./products.js");
+
+const app = express();
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send("Api is Running..");
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT} in ${process.env.MODE} mode`);
+});
