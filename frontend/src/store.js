7d95a3d9d2d837bbc852586be0ade7f97fcccfb3
shubham kumar <shubhofficial1@gmail.com>
2022-12-08 00:24:33 +0530

    create Redux Store

---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import thunk from "redux-thunk";
+
+const store = configureStore({
+  reducer: {},
+  preloadedState: {},
+  middleware: [thunk],
+});
+
+export default store;
